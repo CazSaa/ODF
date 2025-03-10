0f9d71e4c6f05bd9957eedd7e70ba406ae85294a
CazSaa <c.saaltink@student.utwente.nl>
2025-03-10 10:12:34 +0100

    fix(L1BDD): properly include conditions in all nodes

--- odf/checker/layer1/layer1_bdd.py
+++ odf/checker/layer1/layer1_bdd.py
@@ -2,7 +2,7 @@ from dd import cudd
 from lark import Transformer, Visitor, Tree
 from lark.visitors import _Leaf_T, _Return_T
 
-from odf.models.disruption_tree import DisruptionTree
+from odf.models.disruption_tree import DisruptionTree, DTNode
 from odf.models.object_graph import ObjectGraph
 
 
@@ -24,11 +24,13 @@ class Layer1FormulaVisitor(Visitor):
             (self.attack_tree, self.attack_nodes),
             (self.fault_tree, self.fault_nodes)]:
             if disruption_tree.has_node(node_name):
-                collection.update(disruption_tree.get_basic_descendants(node_name))
+                collection.update(
+                    disruption_tree.get_basic_descendants(node_name))
 
                 for descendant in disruption_tree.get_descendants(node_name):
                     self.object_properties.update(
-                        disruption_tree.nodes[descendant]["data"].object_properties)
+                        disruption_tree.nodes[descendant][
+                            "data"].object_properties)
                 return
 
         if self.object_graph.has_object_property(node_name):
@@ -69,14 +71,18 @@ class ConditionTransformer(Transformer):
         return ~items[0]
 
 
-def intermediate_node_to_bdd(bdd: cudd.BDD, disruption_tree: DisruptionTree, node_name: str) -> cudd.Function:
+def basic_node_to_bdd(bdd: cudd.BDD, node: DTNode):
+    if node.condition_tree is None:
+        return bdd.var(node.name)
+    condition_bdd = ConditionTransformer(bdd).transform(node.condition_tree)
+    return bdd.var(node.name) & condition_bdd
+
+
+def intermediate_node_to_bdd(bdd: cudd.BDD, disruption_tree: DisruptionTree,
+                             node_name: str) -> cudd.Function:
     node = disruption_tree.nodes[node_name]["data"]
     if disruption_tree.out_degree(node_name) == 0:
-        if node.condition_tree is None:
-            return bdd.var(node_name)
-
-        condition_bdd = ConditionTransformer(bdd).transform(node.condition_tree)
-        return bdd.var(node_name) & condition_bdd
+        return basic_node_to_bdd(bdd, node)
 
     children = list(disruption_tree.successors(node_name))
 
@@ -88,14 +94,23 @@ def intermediate_node_to_bdd(bdd: cudd.BDD, disruption_tree: DisruptionTree, nod
 
     result = intermediate_node_to_bdd(bdd, disruption_tree, children[0])
     for child in children[1:]:
-        result = bdd.apply(apply, result, intermediate_node_to_bdd(bdd, disruption_tree, child))
+        result = bdd.apply(apply, result,
+                           intermediate_node_to_bdd(bdd, disruption_tree,
+                                                    child))
 
-    return result
+    if node.condition_tree is None:
+        return result
+    condition_bdd = ConditionTransformer(bdd).transform(node.condition_tree)
+    return result & condition_bdd
 
 
 # noinspection PyMethodMayBeStatic
 class Layer1BDDTransformer(Transformer):
     """Transforms a layer 1 formula parse tree into a BDD."""
+
+    attack_nodes: set[str]
+    fault_nodes: set[str]
+    object_properties: set[str]
 
     def __init__(self,
                  attack_tree: DisruptionTree,
@@ -112,13 +127,16 @@ class Layer1BDDTransformer(Transformer):
                                        self.object_graph)
         visitor.visit(tree)
 
+        self.attack_nodes = visitor.attack_nodes
+        self.fault_nodes = visitor.fault_nodes
+        self.object_properties = visitor.object_properties
+
         self.bdd.declare(*visitor.object_properties, *visitor.fault_nodes,
                          *visitor.attack_nodes)
-        super().transform(tree)
+        return super().transform(tree)
 
     def with_boolean_evidence(self, items):
         raise NotImplementedError()
-
 
     def impl_formula(self, items):
         formula1, formula2 = items
@@ -146,13 +164,22 @@ class Layer1BDDTransformer(Transformer):
         node_name = items[0].value
 
         if node_name in self.bdd.vars:
-            # A basic node or object property
-            # fixme does not take object properties into account
-            return self.bdd.var(node_name)
+            if node_name in self.object_properties:
+                return self.bdd.var(node_name)
+
+            if node_name in self.attack_nodes:
+                node = self.attack_tree.nodes[node_name]["data"]
+                return basic_node_to_bdd(self.bdd, node)
+            if node_name in self.fault_nodes:
+                node = self.fault_tree.nodes[node_name]["data"]
+                return basic_node_to_bdd(self.bdd, node)
+
+            raise ValueError(f"Unknown node: {node_name}")
 
         for disruption_tree in [self.attack_tree, self.fault_tree]:
             if disruption_tree.has_intermediate_node(node_name):
-                return intermediate_node_to_bdd(self.bdd, disruption_tree, node_name)
+                return intermediate_node_to_bdd(self.bdd, disruption_tree,
+                                                node_name)
 
         if node_name not in self.bdd.vars:
             raise ValueError(f"Unknown node: {node_name}")
